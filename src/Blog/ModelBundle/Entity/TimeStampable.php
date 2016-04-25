988190cfc09c8205107394488fac9c014344f3e4
Tomasz <tomazi2010@gmail.com>
2016-04-25 17:35:11 +0100

    Section 5 First Migration

---
+++ src/Blog/ModelBundle/Entity/TimeStampable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Blog\ModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Timestampable abstract class to define created behaviour
+ * @ORM\MappedSuperClass
+ */
+abstract class TimeStampable
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Author
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+} 
